7646d5eae34598a8b20bc7f7e31c3b635db021a2
Keith Benedict <kbenedict@postmedia.com>
2014-09-05 16:05:27 -0400

    revert change on install script, added to history file

--- tests/install.sh
+++ tests/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -ev
 CURRENT_FOLDER="$(basename "$(pwd)")"
-#svn co http://develop.svn.wordpress.org/trunk/ ./
+
 if [ "tests" = $CURRENT_FOLDER ]; then
 	echo "Retrieving wordpress trunk files...."
-	svn co http://develop.svn.wordpress.org/trunk/tests ./tests
+	svn co http://develop.svn.wordpress.org/trunk/ ./
 	
 	echo "copying the sample test file"
 	cp wp-tests-config-sample.php wp-tests-config.php
@@ -12,7 +12,7 @@ if [ "tests" = $CURRENT_FOLDER ]; then
 	echo "Inserting travis-ci specific credentials for mysql"
 	sed -i ".bak" -f sed_commands wp-tests-config.php
 	rm wp-tests-config.php.bak
-	WP_CORE_DIR=/tmp/wordpress/
+
 else
 	echo "Please execute from within the tests folder!"
 fi
