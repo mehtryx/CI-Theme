c2b92ed0c090ce499c01b2e2c1e60397cf306cef
Keith Benedict <kbenedict@postmedia.com>
2014-09-08 09:39:01 -0400

    updated to include code sniffing

--- tests/install.sh
+++ tests/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -ev
 CURRENT_FOLDER="$(basename "$(pwd)")"
+TEST_PATH = "$(pwd)"
 
 if [ "tests" = $CURRENT_FOLDER ]; then
 	echo "Retrieving wordpress trunk files...."
@@ -12,8 +13,25 @@ if [ "tests" = $CURRENT_FOLDER ]; then
 	echo "Inserting travis-ci specific credentials for mysql"
 	
 	# sed works differently on osx than linux, detect if on travis and run correct command
+	# further adding code sniffing on travis, have not tested on osx to include genericly
 	if [ "$TRAVIS" = true ]; then
 		sed -i -f sed_commands wp-tests-config.php
+		
+		# Install CodeSniffer for WordPress Coding Standards checks.
+		cd /tmp/wordpress
+		git clone --quiet https://github.com/squizlabs/PHP_CodeSniffer.git php-codesniffer
+		# Install WordPress Coding Standards.
+		git clone --quiet https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git wordpress-coding-standards
+		# Hop into CodeSniffer directory.
+		cd php-codesniffer
+		# Set install path for WordPress Coding Standards
+		# @link https://github.com/squizlabs/PHP_CodeSniffer/blob/4237c2fc98cc838730b76ee9cee316f99286a2a7/CodeSniffer.php#L1941
+		./scripts/phpcs --config-set installed_paths ../wordpress-coding-standards
+		# Hop into themes directory.
+		cd $TEST_PATH
+		# After CodeSniffer install you should refresh your path.
+		phpenv rehash
+		
 	else
 		sed -i ".bak" -f sed_commands wp-tests-config.php
 		rm wp-tests-config.php.bak
