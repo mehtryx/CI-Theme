b54f408cacaa677006e014fa69f0b74d5ca4a4fd
Keith Benedict <keith@benedicthome.com>
2015-01-12 10:55:09 -0500

    fixing whitespace

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,13 +4,13 @@ $path = dirname( __FILE__ ) . '/tests/phpunit/includes/bootstrap.php';
 
 // We check if the path is valid, if so then we can define globals and proceed.
 if ( file_exists( $path ) ) {
-	
+
 	// activation of theme or plugins would happen here, at this point you can define
-	// $GLOBALS['wp_tests_options'] = array() 
+	// $GLOBALS['wp_tests_options'] = array()
 	// the array contains key/value pairs such as:
 	//
 	// 'active_plugins' => array( 'wp-iron-io/wp-iron-io.php' )
-	
+
 	// Make sure this is the last line, as the environment will now initialize
 	require_once $path;
 } else {
